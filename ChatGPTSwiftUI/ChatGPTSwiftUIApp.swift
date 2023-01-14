2e2b142eb9b23492ca55aab5daf18b12aac4428e
Aung Ko Min <aung_ko_min@transitlink.com.sg>
2023-01-15 03:26:09 +0800

    Initial Commit

---
+++ ChatGPTSwiftUI/ChatGPTSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTSwiftUIApp.swift
+//  ChatGPTSwiftUI
+//
+//  Created by Aung Ko Min on 15/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatGPTSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ChatGPTSwiftUI
+//
+//  Created by Aung Ko Min on 15/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
